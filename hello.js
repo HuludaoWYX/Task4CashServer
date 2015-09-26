c9c1040b0834f370208a32e3f52b570e44df34b8
ywang751 <ywang751@gatech.edu>
2015-09-26 13:54:43 -0400

    cssstill

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/split.html'));
@@ -16,4 +16,4 @@ app.get('/', function (req, res) {
 
 
 app.listen(3000);
-console.log("Running on 3000");
+console.log("Running on 3000")
