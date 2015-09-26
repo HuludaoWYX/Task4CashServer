ed9dc3c7817fca295dc1778caf98030aece40a89
ywang751 <ywang751@gatech.edu>
2015-09-26 02:29:32 -0400

    third

--- hello.js
+++ hello.js
@@ -1,11 +1,19 @@
-var http = require('http');
+var http = require('http')
+ ,  fs = require('fs');
 
-http.createServer(function(request, response) {
-	response.writeHead("this is head");
-	response.write("Hello World");
-	setTimeout(function() {
-		response.write("running done");
-		response.end();
-	}, 5000);
-}).listen(8080);
+
+fs.readFile('./split.html', function(err, html) {
+	http.createServer(function(request, response) {
+		response.writeHead("this is head");
+		response.write("Hello World");
+		// setTimeout(function() {
+		// 	response.write("running done");
+		// 	response.end();
+		// }, 5000);
+	}).listen(8080);
+
+
+}
+
+
 console.log('Listening on port 8080...');
