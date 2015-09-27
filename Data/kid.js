604448040a08f94d0f1d547a906630355039a281
ywang751 <ywang751@gatech.edu>
2015-09-27 00:18:34 -0400

    MongoDBIntegrated

--- Data/kid.js
+++ Data/kid.js
@@ -1,2 +1,19 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/adult');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/kid');
+
+var kidSchema = new Schema({
+	name: {type: String, required: true},
+
+	age: {type: String, required: true},
+	gender: {type: String, required: true},
+	bio: String,
+	phoneNum: String,
+	email: {type: String, required: true, unique: true},
+	pw: {type: String, required: true},
+	attitude: Number,
+	punctual: Number,
+	quality: Number
+});
+
+mongoose.model('kid', kidSchema);
--- hello.js
+++ hello.js
@@ -1,24 +1,43 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var db = require('./Data/db');
-
+var db = require('./Data/kid');
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/adult", function(err) {
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect("mongodb://localhost/kid", function(err) {
 	console.log("Connected");
 });
 
-mongoose.model('kid', {name: String});
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+
+var kidMod = mongoose.model('kid');
+
+var kid1 = new kidMod({name: "John", age: "15", gender: "Male", email: "John@gmail.com", pw: "123"});
+var kid2 = new kidMod({name: "Alice", age: "17", gender: "Female", email: "Ailce@hotmail.com", pw: "123"})
+
+kid2.save(function (err) {
+	if (err) return null;
+});
+
+kid1.save(function (err) {
+	if (err) return null;
+});
 app.get('/', function (req, res) {
   //res.sendFile(path.join(__dirname+'/split.html'));
-  mongoose.model('kid').find(function(err, kid){
+
+  // mongoose.model('kid').find(function(err, kid){
+  // 	if (kid.name = "Alice") {
+  // 		res.send(kid);
+  // 	}
+  	//res.send(kid);
+  
+  kidMod.find({name: "Alice"}, function(err, kid) {
   	res.send(kid);
   });
-
 
 });
 
