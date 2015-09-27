00ece0af3d18dc88f8d47756f5a0717c5f15e82f
ywang751 <ywang751@gatech.edu>
2015-09-27 02:26:25 -0400

    DatabaseUp

--- hello.js
+++ hello.js
@@ -3,9 +3,13 @@ var app = express();
 var path = require('path');
 var db = require('./Data/kid');
 var mongoose = require('mongoose');
-
+var http = require('http');
+var bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use();
+
+
 mongoose.connect("mongodb://localhost/kid", function(err) {
 	console.log("Connected");
 });
@@ -17,7 +21,7 @@ mongoose.connect("mongodb://localhost/kid", function(err) {
 var kidMod = mongoose.model('kid');
 
 var kid1 = new kidMod({name: "John", age: "15", gender: "Male", email: "John@gmail.com", pw: "123"});
-var kid2 = new kidMod({name: "Alice", age: "17", gender: "Female", email: "Ailce@hotmail.com", pw: "123"})
+var kid2 = new kidMod({name: "Alice", age: "17", gender: "Female", email: "Ailce@hotmail.com", pw: "123"});
 
 kid2.save(function (err) {
 	if (err) return null;
@@ -26,8 +30,10 @@ kid2.save(function (err) {
 kid1.save(function (err) {
 	if (err) return null;
 });
+
+
 app.get('/', function (req, res) {
-  //res.sendFile(path.join(__dirname+'/split.html'));
+  res.sendFile(path.join(__dirname+'/split.html'));
 
   // mongoose.model('kid').find(function(err, kid){
   // 	if (kid.name = "Alice") {
@@ -35,12 +41,25 @@ app.get('/', function (req, res) {
   // 	}
   	//res.send(kid);
   
-  kidMod.find({name: "Alice"}, function(err, kid) {
-  	res.send(kid);
-  });
+  // kidMod.find({name: "Alice"}, function(err, kid) {
+  // 	res.send(kid);
+  // });
 
 });
 
+app.post('/login', function (req, res) {
+	
+	res.sendFile(path.join(__dirname+'/login.html'));
+	console.log(req.field);
+
+});
+
+app.get('/login', function (req, res) {
+	
+	res.sendFile(path.join(__dirname+'/login.html'));
+
+
+});
 
 app.listen(8080);
 console.log("Running on 8080")
