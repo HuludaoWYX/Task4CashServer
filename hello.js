774b196131e82e1110a91ee8bded04e244e9b70f
ywang751 <ywang751@gatech.edu>
2015-09-26 21:44:15 -0400

    dbSetUp

--- hello.js
+++ hello.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var db = require('./Data/db');
+
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/adult", function(err) {
+	console.log("Connected");
+});
+
+mongoose.model('kid', {name: String});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/split.html'));
+  //res.sendFile(path.join(__dirname+'/split.html'));
+  mongoose.model('kid').find(function(err, kid){
+  	res.send(kid);
+  });
+
+
 });
 
 
-
-
-
-
-
-
 app.listen(8080);
-console.log("Running on 3000")
+console.log("Running on 8080")
 console.log(__dirname);
+
